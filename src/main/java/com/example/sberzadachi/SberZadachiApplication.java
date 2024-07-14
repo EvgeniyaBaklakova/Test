abada38aaaa889efd0df4a41a46f4c465d760252
Geny <Geny1977@yandex.ru>
2024-07-14 16:06:53 +0300

    Commit one

---
+++ src/main/java/com/example/sberzadachi/SberZadachiApplication.java
@@ -0,0 +1,14 @@
+package com.example.sberzadachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberZadachiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberZadachiApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/sberzadachi/SberZadachiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sberzadachi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberZadachiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
